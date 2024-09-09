58759ae413503bbb46b5667827bc41e125e52a4c
victorcolonfigueroa <victor.colonfigueroa@gmail.com>
2024-09-09 10:04:15 -0400

    chore: Refactor FizzBuzz function to improve readability and remove unnecessary comments

--- challenge/0-fizzbuzz.py
+++ challenge/0-fizzbuzz.py
@@ -3,13 +3,11 @@
 """
 import sys
 
-
 def fizzbuzz(n):
     """
-    FizzBuzz function prints numbers from 1 to n separated by a space.
-
-    - For multiples of three print "Fizz" instead of the number and for
-      multiples of five print "Buzz".
+    Print the numbers from 1 to n separated by a space.
+    - For multiples of three print "Fizz" instead of the number.
+    - For multiples of five print "Buzz".
     - For numbers which are multiples of both three and five print "FizzBuzz".
     """
     if n < 1:
